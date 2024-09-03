0c75784bbb9be1eb339f09fee876f6b666ca2e5d
SamsTheNerd <sam@samsthenerd.com>
2024-09-03 19:04:35 -0400

    fix neoforge client init and make winged sword actually apply knockback

--- common/src/main/java/com/samsthenerd/monthofswords/items/WingSwordItem.java
+++ common/src/main/java/com/samsthenerd/monthofswords/items/WingSwordItem.java
@@ -9,13 +9,14 @@ import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.math.MathHelper;
 
 public class WingSwordItem extends SwordItem {
-    public static final ClassyToolMaterial WING_MATERIAL = new ClassyToolMaterial(2031, 12f, 0,
+    public static final ClassyToolMaterial WING_MATERIAL = new ClassyToolMaterial(2031, 12f, 0.1f,
             BlockTags.INCORRECT_FOR_GOLD_TOOL, 22, () -> Ingredient.EMPTY);
 
     public WingSwordItem(Item.Settings itemSettings){
         super(WING_MATERIAL, itemSettings.attributeModifiers(SwordItem.createAttributeModifiers(WING_MATERIAL, -1, 0)));
     }
 
+    @Override
     public void postDamageEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         // nop, don't take durability.
         target.takeKnockback(1,
--- neoforge/src/main/java/com/samsthenerd/monthofswords/neoforge/SwordsModNeoForgeClient.java
+++ neoforge/src/main/java/com/samsthenerd/monthofswords/neoforge/SwordsModNeoForgeClient.java
@@ -1,12 +1,15 @@
 package com.samsthenerd.monthofswords.neoforge;
 
 import com.samsthenerd.monthofswords.SwordsModClient;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
+@EventBusSubscriber(modid = "monthofswords", value= Dist.CLIENT, bus= EventBusSubscriber.Bus.MOD)
 public class SwordsModNeoForgeClient {
     @SubscribeEvent
-    public void clientSetup(FMLClientSetupEvent event){
+    public static void clientSetup(FMLClientSetupEvent event){
         SwordsModClient.init();
     }
 }
